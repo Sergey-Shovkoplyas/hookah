21e1414fe80f3840c77adf3e00b9bda2df4ff34a
Sergey Shovkoplyas <shovkoplyassergey@gmail.com>
2020-02-12 04:38:07 +0200

    Swiper add

--- app/js/common.js
+++ app/js/common.js
@@ -7,16 +7,22 @@
 $(document).ready(function () {
 
 	let mySwiper = new Swiper('.swiper-container', {
-		// Optional parameters
-		initialSlide: 1,
+		// initialSlide: 1,
 		slidesPerView: 'auto',
 		centeredSlides: true,
-		spaceBetween: 117,
+		spaceBetween: 20,
 		navigation: {
 			nextEl: '.swiper-button-next',
 			prevEl: '.swiper-button-prev',
+		},
+		breakpoints: {
+			768: {
+				spaceBetween: 110
+			}
 		}
 	})
+
+	mySwiper.slideTo(1, 0, false);
 
 	console.log('JQuery!!!');
 
